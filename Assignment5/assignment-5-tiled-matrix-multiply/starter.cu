f92b447f3d1318ef9d37263527a54dcabe4d8df6
sgabriel92 <seb.gab1992@gmail.com>
2024-04-28 13:56:46 -0700

    update

--- Assignment5/assignment-5-tiled-matrix-multiply/starter.cu
+++ Assignment5/assignment-5-tiled-matrix-multiply/starter.cu
@@ -17,7 +17,6 @@ __global__ void matrixMultiplyShared(float *A, float *B, float *C,
                                      int numARows, int numAColumns,
                                      int numBRows, int numBColumns,
                                      int numCRows, int numCColumns) {
-  //@@ Insert code to implement matrix multiplication here
   //@@ You have to use shared memory for this MP
   __shared__ float ds_A[16][16];
   __shared__ float ds_B[16][16];
